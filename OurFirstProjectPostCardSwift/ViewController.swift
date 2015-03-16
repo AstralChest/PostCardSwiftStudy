38a06c0dc386a2683e29ccb1177bf4b6703f6d1e
Tauan Flores <tauanflores@Tauans-MacBook-Pro.local>
2015-03-16 17:34:34 -0300

    Initial Commit

---
+++ OurFirstProjectPostCardSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OurFirstProjectPostCardSwift
+//
+//  Created by Tauan Flores on 3/16/15.
+//  Copyright (c) 2015 Astral Chest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
